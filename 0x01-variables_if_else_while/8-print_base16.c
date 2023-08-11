e42d63a013a4200d6baf1d1b058caed47e000019
carolchim <gracioustempest18@gmail.com>
2023-08-11 02:45:37 -0700

    numbers and letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - main is entry point
+  * Description: 'print numbers of base 16'
+  * Return: 0 Success
+  */
+int main(void)
+{
+	int m;
+	int n;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
