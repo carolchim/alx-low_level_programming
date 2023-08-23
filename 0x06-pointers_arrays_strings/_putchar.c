d64ed903116f1675be03c65efcacb959704ab7be
carolchim <gracioustempest18@gmail.com>
2023-08-23 11:05:10 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+  * _putchar - writes the character c to stdio
+  * @c: the character to print
+  * Return: 0n success 1
+  * On error, -1 is returned, and errno is set appropriately
+  */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
